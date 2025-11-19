489f4a00d84d294d9d5872c51553c09e6f75b99c
oguzbayoguz-bay <oguzbayoguz@gmail.com>
2025-11-19 14:25:21 +0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('calc-v1').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      return resp || fetch(e.request);
+    })
+  );
+});
